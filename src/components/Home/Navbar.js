138adfdfc21b391992fb950b9c0d730aba064705
sudama dhakad <sdhakad011@gmail.com>
2022-11-07 19:48:56 +0530

    navbar and footer dark

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,13 +1,13 @@
-import React,{useContext} from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import logo from './logo-criclive.png'
 import { getAuth, signOut } from "firebase/auth";
 import { UserContext } from '../../context/userContext';
 
 export default function Navbar() {
-  
+    const location = useLocation();
     const { user } = useContext(UserContext);
-    
+
     const navigate = useNavigate();
     const auth = getAuth();
 
@@ -24,7 +24,7 @@ export default function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-            <img src={logo} className="rounded" alt="criclive" style={{ height: "50px" }} />
+            <img src={logo} className="bg-warning" alt="criclive" style={{ height: "40px", width: "60px" }} />
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,22 +34,22 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto ">
 
-                    <li className="nav-item active" style={{ margin: "6px" }}>
-                        <Link className="nav-link active" to='/'>Home</Link>
-                    </li>
+                    {!user.name && <li className="nav-item" style={{ margin: "6px" }}>
+                        <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} to='/'>Home</Link>
+                    </li>}
 
                     {!user.name &&
                         < li className="nav-item" style={{ margin: "6px" }}>
-                            <Link className="nav-link active" to="/login">Login</Link>
+                            <Link className={`nav-link ${location.pathname === '/login' ? 'active' : ''}`} to="/login">Login</Link>
                         </li>}
 
 
                     {user.name && <li className="nav-item" style={{ margin: "6px" }} >
-                        <Link className="nav-link active" onClick={handleLogout} >Logout</Link>
+                        <Link className={`nav-link ${location.pathname === '/logout' ? 'active' : ''}`} to="/logout" onClick={handleLogout} >Logout</Link>
                     </li>}
 
                     {user.name && <li className="nav-item" style={{ margin: "6px" }}>
-                        <Link className="nav-link active" to="/profile">Profile</Link>
+                        <Link className={`nav-link ${location.pathname === '/profile' ? 'active' : ''}`} to="/profile">Profile</Link>
                     </li>}
 
                 </ul>
